ed1cd8531d0fe27fedadc2dbecf68d5fbc62638b
Tarel-Github <sms_stmax@naver.com>
2023-02-20 10:40:31 +0900

    답안수정

--- Day06/ct07_bj1253.py
+++ Day06/ct07_bj1253.py
@@ -11,18 +11,17 @@ A.sort()
 
 for k in range(N):
     find = A[k]
-    i = 0; j =N-1
-    while i< j:
+    i = 0; j = N - 1
+    while i < j:
         if A[i] + A[j] == find:
             if i != k and j != k:
                 count += 1
                 break
-            elif i == k : j += 1
+            elif i == k : i += 1
             elif j == k : j -= 1
         elif A[i] + A[j] < find:
             i += 1
         elif A[i] + A[j] > find:
             j -= 1
-
-
+ 
 print(count)
