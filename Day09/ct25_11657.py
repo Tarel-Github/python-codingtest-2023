300cf32e435986c334793fccc3a3365b805bf7ed
Tarel-Github <sms_stmax@naver.com>
2023-02-23 14:02:08 +0900

    알고리즘

--- Day09/ct25_11657.py
+++ Day09/ct25_11657.py
@@ -6,7 +6,7 @@ N, M = map(int, input().split())
 edges = []
 distance = [sys.maxsize] * (N + 1) # 파이썬 시스템이 가질 수 있는 최대값을 배열로 저장, 이때 배열의 요소수는 N+1 개
 
-# 3개의 숫자를 받아서 start, end, time에 할당하고 이를 M만큼 반복
+# 3개의 숫자를 받아서 start, end, time에 할당하고, 다시 그걸 edges라는 배열에 할당, 이를 M(엣지)만큼 반복
 for _ in range(M):
     start, end, time = map(int, input().split())
     edges.append((start, end, time))
@@ -14,12 +14,13 @@ for _ in range(M):
 # 벨만포드 수행
 distance[1] = 0 # distance의 1번 요소를 0으로 변경
 
+# 엣지에 저장된 숫자들을 다시 start, end, time에 할당하면서, 
 for _ in range(N-1):
     for start, end, time in edges:
         if distance[start] != sys.maxsize and distance[end] > distance[start] + time:
             distance[end] = distance[start] + time
 
-#음수 사이클
+# 음수 사이클
 mCycle = False
 for start, end, time in edges:
     if distance[start] != sys.maxsize and distance[end] > distance[start] + time:
