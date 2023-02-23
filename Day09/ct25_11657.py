e1ef10d685997d1676c45d44e38ae355834b554a
Tarel-Github <sms_stmax@naver.com>
2023-02-23 10:50:03 +0900

    최단거리 알고리즘

---
+++ Day09/ct25_11657.py
@@ -0,0 +1,36 @@
+# 백준 11657 - 타임머신
+import sys
+
+# 두 숫자를 입력받아 N과 M에 할당, 각각 노드와 엣지임
+N, M = map(int, input().split())
+edges = []
+distance = [sys.maxsize] * (N + 1) # 파이썬 시스템이 가질 수 있는 최대값을 배열로 저장, 이때 배열의 요소수는 N+1 개
+
+# 3개의 숫자를 받아서 start, end, time에 할당하고 이를 M만큼 반복
+for _ in range(M):
+    start, end, time = map(int, input().split())
+    edges.append((start, end, time))
+
+# 벨만포드 수행
+distance[1] = 0 # distance의 1번 요소를 0으로 변경
+
+for _ in range(N-1):
+    for start, end, time in edges:
+        if distance[start] != sys.maxsize and distance[end] > distance[start] + time:
+            distance[end] = distance[start] + time
+
+#음수 사이클
+mCycle = False
+for start, end, time in edges:
+    if distance[start] != sys.maxsize and distance[end] > distance[start] + time:
+        mCycle = True #음수 사이클이 존재!
+        break
+
+if mCycle != True:
+    for i in range(2, N+1):
+        if distance[i] != sys.maxsize:
+            print(distance[i])
+        else:
+            print(-1)
+else:
+    print(-1)
