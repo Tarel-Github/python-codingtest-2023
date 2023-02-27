6bc1a7878b69d4bcd627e40c7ef3624768a16099
Tarel-Github <sms_stmax@naver.com>
2023-02-27 09:48:31 +0900

    마지막 수정

--- CodingTest/backup.py
+++ CodingTest/backup.py
@@ -1,19 +1,7 @@
-def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
-    answer = n - len(lost)
-    for i in range(0, len(lost)):
-        if lost[i] in reserve:
-            reserve.remove(lost[i])
-            lost[i] = 99
-            answer += 1
-    for i in range(0, len(lost)):
-        if lost[i]-1 in reserve:
-            reserve.remove(lost[i]-1)
-            lost[i] = 99
-            answer += 1    
-        elif lost[i]+1 in reserve:
-            reserve.remove(lost[i]+1)
-            lost[i] = 99
-            answer += 1
-    return answer
+
+
+
+
+
+
+
